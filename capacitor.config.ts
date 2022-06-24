ea24a3a704c7a0494c91d0bed592f2d5180d901c
Cristhian M <linkincypher@hotmail.com>
2022-06-24 16:55:17 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-sistema',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
